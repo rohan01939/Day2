ac66645ff7153887e3a86191a8b063658aa368d2
rohan01939 <rohan01939@gmail.com>
2019-10-01 00:50:04 -0400

    Day2

---
+++ src/Day1.java
@@ -0,0 +1,16 @@
+
+public class Day1 {
+public static void main(String[] args){
+	
+	String a="Rohan";
+	String b="Vdm";
+	String c="Usa";
+	int y=2013;
+	int x=2012;
+	
+	System.out.println("My name is " +a+ ". My friend name is " +b+ ". I came to " +c+ " at " +y+ ". My friend " +b+ " came to " +c+ " at " +x);
+	
+	
+	
+}
+}
